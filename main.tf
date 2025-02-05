a2311c5dcae704b85ab5726d691394c6c78a4de3
Aaron Warner <133413474+awarner23@users.noreply.github.com>
2025-02-05 07:07:07 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
+module "website_s3_bucket" {
+  source = "./modules/aws-s3-static-website-bucket"
+
+  bucket_name = "aaron-warner-02-05-2025"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
 
